bd5899420dde2a3f3699545b6cd359dbfd807d48
Tau <hectauduque@gmail.com>
2021-02-23 21:11:36 -0300

    fim

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about the project</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
+        error illo laudantium non. Rem minima ratione architecto veniam error
+        ipsum, delectus illo veritatis tenetur iste ad dolor exercitationem
+        vitae voluptatem distinctio quam ipsam similique alias.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
